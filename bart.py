2fb8225ebb234c14bcea4b851561c7dbf030ce38
Giacomo Petrillo <info@giacomopetrillo.com>
2022-02-26 21:58:59 +0100

    bart: added posterior on error term sdev

--- bart.py
+++ bart.py
@@ -2,48 +2,71 @@ import functools
 
 import numpy as np
 import numba
-from scipy import stats, linalg
+from scipy import stats, linalg, optimize
 
-def sample_bart_prior(X_data, X_test, y_data, size, *, m=200, alpha=0.95, beta=2, k=2, nu=3, q=0.9, seed=0, check=True):
+default_bart_params = dict(m=200, alpha=0.95, beta=2, k=2, nu=3, q=0.9)
+
+def sample_bart_prior(X_data, X_test, y_data, size, *, seed=0, check=True, error=True, **bart_params):
     """
     Sample from the prior distribution of the model
     
         f(x) = sum_{j=1}^m g(x; T_j, M_j) + eps(x)
     
+    where each g(x; T_j, M_j) is a decision tree with random rules T and random
+    leaves M, and eps(x) is a i.i.d. random error.
+    
     Parameters
     ----------
     X_data : (p, n) array
-        Data, n vectors of p covariates (used for splitting points)
+        Data, n vectors of p covariates. Used for splitting points.
     X_test : (p, N) array
-        Points where the prior is computed
+        Points where the prior is computed.
     y_data : (n,) array
-        Outcome (used for prior on mu_ij|T_j and sigma)
+        Outcome. Used for the hyperparameters of the priors on mu_ij|T_j and
+        sigma.
     size : int
-        Number of generated samples
+        Number of generated samples.
+    seed : int
+        Seed for the random number generator, default 0.
+    check : bool
+        Check arguments, default True.
+    error : bool
+        Wether to sample and add the error term. Default True.
+    
+    Keyword arguments
+    -----------------
+    These are the hyperparameters of the BART. The default values are in the
+    global variable `default_bart_params`.
     m : int
-        Number of trees
+        Number of trees.
     alpha, beta : scalar
-        Parameters of p(T_j), p(node nonterminal) = alpha (1 + d)^(-beta)
+        Parameters of p(T_j), P(node nonterminal) = alpha (1 + d)^(-beta).
     k : scalar
-        Inverse scale for p(mu_ij|T_j)
+        Inverse scale for p(mu_ij|T_j).
     nu : int
-        Degrees of freedom of the chisquare for the prior on the error term
+        Degrees of freedom of the chisquare for the prior on the error term.
     q : scalar
         Order of the quantile of y used to scale the prior on the error term
         sigma2_epsilon = lambda / (chi2_nu/nu), p(sigma2_epsilon < var(y)) = q.
-        Set q = 1 to disable error.
-    seed : int
-        Seed for the random number generator
-    check : bool
-        Check arguments
+        If q = 1 the error is disabled.
     
     Return
     ------
     y_test : (size, N) array
         Samples from the prior on f(X_test)
     """
+    
+    # extract BART hyperparameters
+    params = dict(default_bart_params)
+    params.update(**bart_params)
+    m = params['m']
+    alpha = params['alpha']
+    beta = params['beta']
+    k = params['k']
+    nu = params['nu']
+    q = params['q']
 
-    if check:
+    if check:   # check all arguments
         X_data = np.asarray(X_data)
         p, n = X_data.shape
         assert n >= 2, n
@@ -86,8 +109,11 @@ def sample_bart_prior(X_data, X_test, y_data, size, *, m=200, alpha=0.95, beta=2
     
         assert seed == int(seed)
     
-    seeds = make_seeds(seed, size)
-    return sample_bart_prior_impl(X_data, X_test, y_data, size, m, alpha, beta, k, nu, q, seeds)
+    seeds = make_seeds(seed, size + 1)
+    gen = np.random.default_rng(seeds[0])
+    if not error:
+        q = 1
+    return sample_bart_prior_impl(X_data, X_test, y_data, size, m, alpha, beta, k, nu, q, seeds[1:], gen)
 
 def make_seeds(seed, size):
     s = np.random.SeedSequence(seed)
@@ -96,11 +122,7 @@ def make_seeds(seed, size):
         out[i] = s.spawn(1)[0].pool[0]
     return out
 
-@numba.jit(nopython=True, cache=True)
-def random_normal(shape):
-    return np.random.randn(*shape)
-
-def sample_bart_prior_impl(X_data, X_test, y_data, size, m, alpha, beta, k, nu, q, seeds):
+def sample_bart_prior_impl(X_data, X_test, y_data, size, m, alpha, beta, k, nu, q, seeds, gen):
     p, N = X_test.shape
     y_test = np.zeros((size, N))
     
@@ -123,9 +145,10 @@ def sample_bart_prior_impl(X_data, X_test, y_data, size, m, alpha, beta, k, nu, 
     y_test += mu_mu
     
     # add error term
-    sigma_eps = sigma_epsilon(y_data, nu, q)
-    if sigma_eps != 0:
-        y_test += sigma_eps * random_normal((size, N))
+    dist = var_epsilon_dist(y_data, nu, q)
+    if dist is not None:
+        sigma2_eps = dist.rvs((size, 1), gen)
+        y_test += np.sqrt(sigma2_eps) * gen.standard_normal((size, N))
     
     return y_test
 
@@ -143,16 +166,11 @@ def sample_bart_prior_hellpit(size, m, y_test, sort_map, sp_map, sp_slice, alpha
         # assert np.all(cycle_active_dims == active_dims)
         ## (!) asserts break parallelization
 
-@numba.jit(nopython=True, cache=True)
-def set_seed(seed):
-    np.random.seed(seed)
-
-def sigma_epsilon(y_data, nu, q):
+def var_epsilon_dist(y_data, nu, q):
     hat_sigma2 = np.var(y_data)
     dist = stats.invgamma(a=nu / 2)
     lamda = hat_sigma2 / dist.ppf(q)
-    sigma2 = lamda * dist.rvs()
-    return np.sqrt(sigma2)
+    return None if lamda == 0 else stats.invgamma(a=nu / 2, scale=lamda)
 
 def mumu_sigmamu(y_data, k, m):
     ymin = np.min(y_data)
@@ -260,67 +278,180 @@ def recursive_tree_descent(y_test, d, sort_map, sp_map, sp_slice, alpha, beta, a
 class BartGP:
     
     def __init__(self, **bart_params):
-        self.bart_params = bart_params
+        self.bart_params = dict(default_bart_params)
+        self.bart_params.update(bart_params)
     
-    def fit(self, X_data, y_data, X_test, n_prior_samples=None):
+    def fit(self, X_data, y_data, X_test, sigma=None, n_prior_samples=None):
+        
+        # size variables
         _, N = X_test.shape
-        X_total = np.concatenate([X_test, X_data], axis=1)
+        _, n = X_data.shape
         size = n_prior_samples
         if size is None:
             size = 2 * N
-        y_prior = sample_bart_prior(X_data, X_total, y_data, size, **self.bart_params)
+        elif size <= N:
+            raise ValueError('number of samples {size} shall be greater than number of test points {N}')
+        
+        # sample BART prior
+        X_total = np.concatenate([X_test, X_data], axis=1)
+        y_prior = sample_bart_prior(X_data, X_total, y_data, size, error=False, **self.bart_params)
         
         # split sample
         y_prior_test = y_prior[:, :N]
         y_prior_data = y_prior[:, N:]
         
-        # center mean of the prior on y_data
+        # check mean of the prior on y_data
         mu_mu = (np.max(y_data) + np.min(y_data)) / 2
         dev = np.mean(y_prior_data, axis=0) - mu_mu
-        sigma = np.std(y_prior_data, axis=0) / np.sqrt(size)
-        assert np.all(np.abs(dev) < 5 * sigma)
+        sdev = np.std(y_prior_data, axis=0) / np.sqrt(size)
+        assert np.all(np.abs(dev) < 5 * sdev)
 
         # SVD of prior on y_data
         U, s, Vh = linalg.svd(y_prior_data - mu_mu, full_matrices=False)
-        _, n = X_data.shape
         cut = max(size, n) * np.finfo(float).eps * s[0]
         mask = s > cut
         s = s[mask]
         U = U[:, mask]
+        Vh = Vh[mask, :]
+        s /= np.sqrt(size) # because cov = Y.T @ Y / size
         
-        # compute prior and posterior means and covariance matrices
-        Y = y_prior_test - mu_mu
-        UY = U.T @ Y
-        YY = Y.T @ Y
+        # define function to compute log p(y|sigma^2)
+        Vy = Vh @ (y_data - mu_mu)
+        s2 = s ** 2
+        norm = n * np.log(2 * np.pi)
+        def log_prob_y(sigma2):
+            ss2 = s2 + sigma2
+            logdet = np.sum(np.log(ss2)) + norm
+            quad = (Vy / ss2) @ Vy
+            return -1/2 * (logdet + quad)
+        
+        # define function to compute -log p(y, log(sigma))
+        q = self.bart_params['q']
+        nu = self.bart_params['nu']
+        dist = var_epsilon_dist(y_data, nu, q)
+        def minus_log_prob_y_log_sigma(log_sigma):
+            sigma2 = np.exp(2 * log_sigma)
+            cond = log_prob_y(sigma2)
+            prior = np.log(2) + 2 * log_sigma + dist.logpdf(sigma2)
+            return -cond - prior
+        
+        # fit sigma if not given
+        nosigma = sigma is None
+        if nosigma and dist is None: # BART parameters constrain sigma = 0
+            sigma = 0
+        elif nosigma:
+            
+            # find maximum w.r.t. log(sigma) of p(y, log(sigma))
+            bracket = (
+                1/2 * np.log(dist.interval(0.5)[0]),
+                1/2 * np.log(dist.interval(0.0)[0]),
+            )
+            result = optimize.minimize_scalar(minus_log_prob_y_log_sigma, bracket)
+            if not result.success:
+                raise RuntimeError(result.message)
+            sigma = np.exp(result.x)
+            
+            # Laplace approximation
+            interv = 1/2 * np.log(dist.interval(0.5))
+            eps = (interv[1] - interv[0]) * 1e-4
+            fc = result.fun
+            fr = minus_log_prob_y_log_sigma(result.x + eps)
+            fl = minus_log_prob_y_log_sigma(result.x - eps)
+            deriv = (fr + fl - 2 * fc) / eps ** 2
+            self.y_data_prior_log_marginal_prob = 1/2 * np.log(2 * np.pi / deriv) - fc
+            self.log_sigma_posterior_sdev = 1 / np.sqrt(deriv)
+        
+        # compute prior mean and covariance matrix
+        Y = (y_prior_test - mu_mu) / np.sqrt(size)
         self.y_test_prior_mean = np.full(N, mu_mu)
-        self.y_test_prior_covariance = YY / size
-        self.y_test_posterior_mean = (UY.T / s) @ (Vh @ y_data)
-        self.y_test_posterior_covariance = (YY - UY.T @ UY) / size
-    
-    def sample_prior(self, n_samples, gen=None):
-        return self.sample(n_samples, gen, self.prior_cholesky, self.y_test_prior_mean)
+        self.y_test_prior_covariance = Y.T @ Y
         
-    def sample_posterior(self, n_samples, gen=None):
-        return self.sample(n_samples, gen, self.posterior_cholesky, self.y_test_posterior_mean)
+        # save stuff needed to compute stuff
+        self.s = s
+        self.Vy = Vh @ y_data
+        self.UY = U.T @ Y
+        self.sigma = sigma
+        self.N = N
+        self.dist = dist
+        
+        # compute marginal likelihood
+        self.y_data_prior_log_conditional_prob = log_prob_y(sigma ** 2)
     
-    def sample(self, n_samples, gen, U, mean):
-        samples_iid = self.rnorm(gen, (n_samples, len(U)))
-        return mean + samples_iid @ U
+    def posterior_mean(self, sigma=None):
+        if sigma is None:
+            sigma = self.sigma
+        s = self.s
+        ss2 = s ** 2 + sigma ** 2
+        UY = self.UY
+        Vy = self.Vy
+        return UY.T @ (Vy * s / ss2)
     
-    def rnorm(self, gen, size):
+    def posterior_covariance_matrix(self, sigma=None):
+        if sigma is None:
+            sigma = self.sigma
+        s = self.s
+        ss2 = s ** 2 + sigma ** 2
+        UY = self.UY
+        return self.y_test_prior_covariance - ((UY.T * (s ** 2 / ss2)) @ UY)
+        
+    def _gen(self, gen):
         if gen is None:
-            gen = np.random.default_rng()
+            return np.random.default_rng()
         elif not isinstance(gen, np.random.Generator):
-            gen = np.random.default_rng(gen)
-        return gen.standard_normal(size)
+            return np.random.default_rng(gen)
+        else:
+            return gen
+    
+    def sample_prior(self, n_samples, gen=None, error=False, sample_sigma=True):
+        gen = self._gen(gen)
+        samples = self._sample(n_samples, gen, self.prior_cholesky, self.y_test_prior_mean)
+        if error:
+            sigma = self.sample_sigma_prior((n_samples, 1), gen) if sample_sigma else self.sigma
+            samples += sigma * gen.standard_normal((n_samples, self.N))
+        return samples
+        
+    def sample_posterior(self, n_samples, gen=None, error=False, sample_sigma=None):
+        gen = self._gen(gen)
+        sample_sigma = hasattr(self, 'log_sigma_posterior_sdev') if sample_sigma is None else sample_sigma
+        if sample_sigma:
+            sigmas = self.sample_sigma_posterior(n_samples, gen)
+            samples = np.empty((n_samples, self.N))
+            for i, sigma in enumerate(sigmas):
+                mean = self.posterior_mean(sigma)
+                cov = self.posterior_covariance_matrix(sigma)
+                U = self.cholesky(cov)
+                samples[i] = self._sample(1, gen, U, mean)[0]
+        else:
+            mean = self.posterior_mean()
+            cov = self.posterior_covariance_matrix()
+            U = self.cholesky(cov)
+            samples = self._sample(n_samples, gen, U, mean)
+        if error:
+            sigma = sigmas[:, None] if sample_sigma else self.sigma
+            samples += sigma * gen.standard_normal((n_samples, self.N))
+        return samples
+    
+    def sample_sigma_prior(self, n_samples, gen=None):
+        gen = self._gen(gen)
+        sigma2 = self.dist.rvs(n_samples, gen)
+        return np.sqrt(sigma2)
+    
+    def sample_sigma_posterior(self, n_samples, gen=None):
+        if not hasattr(self, 'log_sigma_posterior_sdev'):
+            raise ValueError('Cannot sample sigma, it was fixed')
+        gen = self._gen(gen)
+        mean = np.log(self.sigma)
+        sdev = self.log_sigma_posterior_sdev
+        log_sigma = mean + sdev * gen.standard_normal(n_samples)
+        return np.exp(log_sigma)
+    
+    def _sample(self, n_samples, gen, U, mean):
+        samples_iid = gen.standard_normal((n_samples, len(U)))
+        return mean + samples_iid @ U
     
     @functools.cached_property
     def prior_cholesky(self):
         return self.cholesky(self.y_test_prior_covariance)
-    
-    @functools.cached_property
-    def posterior_cholesky(self):
-        return self.cholesky(self.y_test_posterior_covariance)
     
     def cholesky(self, M):
         eigmax_bound = np.max(np.sum(np.abs(M), axis=1)) # Gershgorin thorem
@@ -344,17 +475,18 @@ class BartGP:
     def prior_covariance_matrix(self):
         return self.y_test_prior_covariance
     
-    def posterior_mean(self):
-        return self.y_test_posterior_mean
+    def log_marginal_likelihood(self):
+        return self.y_data_prior_log_conditional_prob
     
-    def posterior_covariance_matrix(self):
-        return self.y_test_posterior_covariance
+    def log_hyper_marginal_likelihood(self):
+        attr = 'y_data_prior_log_marginal_prob'
+        return getattr(self, attr, None)
+    
+    def error_log_sdev_posterior_sdev(self):
+        attr = 'log_sigma_posterior_sdev'
+        return getattr(self, attr, None)
     
     # TODO
-    # add error term
-    # method to compute marginal likelihood
-    # method to optimize error term using BART's inverse chisquare error
-    #   prior and marginal likelihood, do laplace to have the full posterior
     # compare with posterior obtained with BayesTree
 
 if __name__ == '__main__':
@@ -365,9 +497,10 @@ if __name__ == '__main__':
     n = 10 # data samples
     N = 1000 # test samples
     size = 100 # prior samples
-    maxplotsamples = 100
-    coord = 0 # plotted coordinate
-    sigma_eps = 0.01 # data error
+    maxplotsamples = 100 # prior samples plotted
+    coord = 0 # plotted X coordinate
+    sigma_eps = 0.1 # data error
+    bart_params = dict(q=0.99) # BART hyperparameters
     
     # generate data from linear model
     gen = np.random.default_rng(202202212324)
@@ -389,29 +522,35 @@ if __name__ == '__main__':
     
     X_test = np.repeat(np.linspace(-0.1, 1.1, N)[None], p, axis=0)
     
-    # generate prior samples
-    bart_params = dict(q=1)
-    y_test_prior = sample_bart_prior(X_data, X_test, y_data, size, seed=gen.integers(2 ** 32), **bart_params)
-    
-    # compute equivalent GP and sample its prior and posterior
+    # compute equivalent GP
     gp = BartGP(**bart_params)
     gp.fit(X_data, y_data, X_test)
-    y_test_prior_gp = gp.sample_prior(size, gen)
-    y_test_posterior_gp = gp.sample_posterior(size, gen)
     
-    fig, axs = plt.subplots(3, 1, num='bart', clear=True, figsize=[8, 7.5], sharex=True, sharey=True)
+    # generate prior and posterior samples
+    samplist = []
+    for error in [False, True]:
+        y_test_prior = sample_bart_prior(X_data, X_test, y_data, size, seed=gen.integers(2 ** 32), error=error, **bart_params)
+        y_test_prior_gp = gp.sample_prior(size, gen, error=error)
+        y_test_posterior_gp = gp.sample_posterior(size, gen, error=error)
+        samplist.append([y_test_prior, y_test_prior_gp, y_test_posterior_gp])
     
-    samplist = [y_test_prior, y_test_prior_gp, y_test_posterior_gp]
-    for ax, samples in zip(axs, samplist):
-        ax.plot(X_data[coord], y_data, '.k', zorder=10)
-        for sample in samples[:maxplotsamples]:
-            ax.plot(X_test[coord], sample, '-r', alpha=0.1)
+    fig, axs = plt.subplots(3, 2, num='bart', clear=True, figsize=[11, 7.5], sharex=True, sharey='col')
     
-    axs[0].set_title('BART prior')
-    axs[1].set_title('Equivalent Gaussian process prior')
-    axs[2].set_title('Equivalent Gaussian process posterior')
-    axs[-1].set_xlabel(f'X[{coord}]')
-    for ax in axs:
+    # plot the samples
+    for axcol, samples in zip(axs.T, samplist):
+        for ax, samp in zip(axcol, samples):
+            ax.plot(X_data[coord], y_data, '.k', zorder=10)
+            for sample in samp[:maxplotsamples]:
+                ax.plot(X_test[coord], sample, '-r', alpha=0.1)
+    
+    axs[0, 0].set_title('BART prior')
+    axs[1, 0].set_title('Equivalent Gaussian process prior')
+    axs[2, 0].set_title('Equivalent Gaussian process posterior')
+    for ax in axs[:, 1]:
+        ax.set_title('(with error term)')
+    for ax in axs[-1]:
+        ax.set_xlabel(f'X[{coord}]')
+    for ax in axs[:, 0]:
         ax.set_ylabel('y')
     
     fig.tight_layout()
